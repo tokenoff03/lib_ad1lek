d41728e6602396fbe9e4ae32b6bcccf0c863e39d
Token Adil <85617958+tokenoff03@users.noreply.github.com>
2025-01-30 16:45:04 +0500

    Redis (#3)
    * Added redis
    
    * HGet add for redis
    
    * done
    
    ---------
    
    Co-authored-by: ad1lek <adil.token@kmf.kz>

--- pkg/cache/client.go
+++ pkg/cache/client.go
@@ -10,6 +10,7 @@ type RedisClient interface {
 	Set(ctx context.Context, key string, value interface{}) error
 	HGetAll(ctx context.Context, key string) (map[string]string, error)
 	Get(ctx context.Context, key string) (string, error)
+	HGet(ctx context.Context, key string, field string) (string, error)
 	Expire(ctx context.Context, key string, expiration time.Duration) error
 	Ping(ctx context.Context) error
 	Close() error
