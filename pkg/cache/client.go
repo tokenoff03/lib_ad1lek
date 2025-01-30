6881747f265929a0ab430265e970fda8a2279202
Token Adil <85617958+tokenoff03@users.noreply.github.com>
2025-01-30 12:39:24 +0500

    Added redis (#1)
    Co-authored-by: ad1lek <adil.token@kmf.kz>

---
+++ pkg/cache/client.go
@@ -0,0 +1,16 @@
+package cache
+
+import (
+	"context"
+	"time"
+)
+
+type RedisClient interface {
+	HashSet(ctx context.Context, key string, values map[string]interface{}) error
+	Set(ctx context.Context, key string, value interface{}) error
+	HGetAll(ctx context.Context, key string) (map[string]string, error)
+	Get(ctx context.Context, key string) (string, error)
+	Expire(ctx context.Context, key string, expiration time.Duration) error
+	Ping(ctx context.Context) error
+	Close() error
+}
